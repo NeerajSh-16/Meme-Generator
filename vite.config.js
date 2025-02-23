c263431932b58de02f5700acab48f4b8f05f6c18
Neeraj.Sh-16 <shneeraj.2619@gmail.com>
2025-02-23 22:06:28 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ['meme-generator-4k27.onrender.com'], // Add your Render domain here
+  },
+});
